df7668be3e614eeedc2e1d25c445b25b51e291ab
Harshkumar Tiwari <41500905+thrix04@users.noreply.github.com>
2022-04-03 17:00:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="mystyle.css">
+<title>Refugee Help</title>
+
+</head>
+<body>
+ <h1>Hello, world!</h1>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="https://ec.europa.eu/info/strategy/priorities-2019-2024/stronger-europe-world/eu-solidarity-ukraine_en">Empower Refugee</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.open.edu/openlearn/supporting-ukrainian-refugees">Courses</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="map.html">Map</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="weather.html">Weather</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<header>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active" style="background-image: url('slide1.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Speak up for Refugees.</h2>
+        </div>
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('slide2.jpg')">
+        <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Don't let hate win</h2>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+  </div>
+</header>
+
+<!-- Page Content -->
+<section class="py-5 text-center">
+      <div class="container"> 
+        <h2 class="text-center" id="services">Services</h2>
+        <p class="text-muted mb-5 text-center">Resources that are available</p>
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 mb-3">
+            <svg class="lnr text-primary services-icon">
+              <use xlink:href="#lnr-magic-wand"></use>
+            </svg>
+           <a href="https://translate.google.com/?sl=auto&tl=en&op=translate"> <h6>Translations</h6></a>
+            <p class="text-muted">Translate one language to another.Also Documents for the ease.</p>
+          </div>
+          <div class="col-sm-6 col-lg-4 mb-3">
+            <svg class="lnr text-primary services-icon">
+              <use xlink:href="#lnr-heart"></use>
+            </svg>
+            <a href="https://www.shondaland.com/act/news-politics/a39283667/10-resources-to-help-ukraine-right-now/"><h6>Donations</h6></a>
+            <p class="text-muted">Funds are made directly and immediately available to the front lines of response.</p>
+          </div>
+          <div class="col-sm-6 col-lg-4 mb-3">
+            <svg class="lnr text-primary services-icon">
+              <use xlink:href="#lnr-rocket"></use>
+            </svg>
+            <a href="https://www.bbc.com/news/world-60525350"><h6>News</h6></a>
+            <p class="text-muted">Latest News, Comprehensive, up-to-date news coverage regarding the Ukraine.</p>
+          </div>
+          <div class="col-sm-6 col-lg-4 mb-3">
+            <svg class="lnr text-primary services-icon">
+              <use xlink:href="#lnr-paperclip"></use>
+            </svg>
+            <a href="https://ec.europa.eu/info/strategy/priorities-2019-2024/stronger-europe-world/eu-solidarity-ukraine/eu-assistance-ukraine/information-people-fleeing-war-ukraine_en"><h6>Rights In Europe</h6></a>
+            <p class="text-muted">Information about your rights with regard to crossing the border into an EU country.</p>
+          </div>
+          <div class="col-sm-6 col-lg-4 mb-3">
+            <svg class="lnr text-primary services-icon">
+              <use xlink:href="#lnr-screen"></use>
+            </svg>
+            <a href="https://liveuamap.com/"><h6>Live Map</h6></a>
+            <p class="text-muted">Real-time location map of the conflict, along with factual news.</p>
+          </div>
+          <div class="col-sm-6 col-lg-4 mb-3">
+            <svg class="lnr text-primary services-icon">
+              <use xlink:href="#lnr-inbox"></use>
+            </svg>
+            <a href="https://www.xe.com/currencyconverter/convert/?Amount=1&From=UAH&To=EUR"><h6><center>Other</center></h6></a>
+            <p class="text-muted">Currency Exchange rates and more.</p>
+          </div>
+        </div>
+      </div>
+</section>
+<section class="main"> 
+   <!-- Header -->
+<header class="bg-primary text-center py-5 mb-4">
+  <div class="container">
+    <a href="https://reliefweb.int/report/ukraine/ukraine-emergency-resources-refugees"><h1 class="font-weight-light text-white">Ukraine Emergency: Resources for Refugees</h1></a>
+  </div>
+</header>
+
+<!-- Page Content -->
+<div class="container">
+  <div class="row">
+    <!-- Team Member 1 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <img src="say.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h1 class="card-title mb-0">SAY</h1>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 2 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <img src="no.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h1 class="card-title mb-0">NO</h1>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 3 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <img src="to.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h1 class="card-title mb-0">TO</h1>
+        </div>
+      </div>
+    </div>
+    <!-- Team Member 4 -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-0 shadow">
+        <img src="war.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+          <h1 class="card-title mb-0">WAR</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.row -->
+
+</div>
+
+<div class="container">
+    <div class="row">
+         <div class="col-md-8">
+        <blockquote class="blockquote text-center mb-0">
+          <p class="mb-0">Peace is the way, Peace is the key, for a better world for you and me.</p>
+        </blockquote>
+        </div>
+  
+        </div>
+        
+      </div>
+
+      <script id="help-ukraine-win" async="true" src="https://helpukrainewinwidget.org/cdn/widget.js" data-type="three" data-position="bottom-right"></script>
+<!-- /.container -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <script src="https://cdn.linearicons.com/free/1.0.0/svgembedder.min.js"></script>
+</body>
+</html>
